c8e3e1f851cc043beb7145459f9e96b14427de27
RosanneVN <annevazquez05@gmail.com>
2024-03-18 18:12:52 -0400

    add image cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import "./App.css";
 
 function App() {
   return (
-    <main>
-      <div className="buttons">
-        <div className="comb-content">
-          <button className="comb-button">COMBINACIONES</button>
-        </div>
-        <div className="game-button">
+   
+       <main>
+        <div className="buttons">
+          <div className="comb-content">
+            <button className="comb-button">COMBINACIONES</button>
+          </div>
+          <div className="game-button">
             <button className="retinarse-button">RETIRARSE</button>
             <button className="igualar-button">IGUALAR</button>
             <button className="subir-button">SUBIR</button>
           </div>
-      </div>
-    </main>
+        </div>
+      </main>
+    
   );
 }
 export default App;
